af0aaf76eca010b221e8057a849046c4a4d5da1e
Thomas <thom.laffay@gmail.com>
2024-09-26 15:26:04 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -1,3 +1,24 @@
 import streamlit as st
+from openai import OpenAI
+
+def prompt_dall_e():
+
+    client = OpenAI(api_key=OpenAIKEY)
+
+    prompt = prompt
+
+    image_cree = client.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        size="512x512",
+        quality="standard",
+        n=1,
+    )
+
+    st.image(image_cree)
 
 st.title("Le truc Dall-E là")
+
+prompt = st.text_input("Veuillez entrer un prompt pour créer une image", on_change=prompt_dall_e())
+OpenAIKEY = st.text_input("Entrez votre clé API")
+
