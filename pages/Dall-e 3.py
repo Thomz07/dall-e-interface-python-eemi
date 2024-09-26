4ab3f62f1ac4c6ddc31ea4f42b9406bef20fe68b
Thomas <thom.laffay@gmail.com>
2024-09-26 15:34:03 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -1,18 +1,20 @@
 import streamlit as st
 import openai
 
-def prompt_dall_e():
-    OpenAIKEY = st.session_state['input_OpenAIKEY']
-    prompt = st.session_state['prompt']
+st.title("Le truc Dall-E là")
 
-    if not OpenAIKEY:
+input_OpenAIKEY = st.text_input("Entrez votre clé API", type="password")
+prompt = st.text_input("Veuillez entrer un prompt pour créer une image")
+
+def prompt_dall_e():
+    openai.api_key = input_OpenAIKEY
+
+    if not openai.api_key:
         st.error("Veuillez entrer votre clé API.")
         return
     if not prompt:
         st.error("Veuillez entrer un prompt.")
         return
-
-    openai.api_key = OpenAIKEY
 
     try:
         response = openai.Image.create(
@@ -21,12 +23,9 @@ def prompt_dall_e():
             size="512x512",
         )
         image_url = response['data'][0]['url']
-        st.image(image_url, caption="Image générée par DALL-E 3")
+        st.image(image_url, caption="Image générée par DALL·E")
     except Exception as e:
         st.error(f"Erreur lors de la génération de l'image: {e}")
 
-st.title("Le truc Dall-E là")
-
-# Use 'key' to store the input values in the session state
-st.text_input("Entrez votre clé API", key='input_OpenAIKEY', type='password')
-st.text_input("Veuillez entrer un prompt pour créer une image", key='prompt', on_change=prompt_dall_e)
+if st.button("Générer l'image"):
+    prompt_dall_e()
