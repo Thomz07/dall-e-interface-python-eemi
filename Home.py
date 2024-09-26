1381815b2d2964e970e1b42fd088705ef185e456
Thomas <thom.laffay@gmail.com>
2024-09-26 14:17:15 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title("Mon :blue[_formulaire_] super cool :sunglasses:")
 st.write("Ceci est un formulaire de contact")
-st.text_input("Tapez votre prompt")
+st.text_input("Tapez votre prompt", placeholder="Un magnifique kebab salade tomate oignon sauce algérienne biggy")
