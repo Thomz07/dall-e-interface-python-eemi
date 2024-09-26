18dde45e96071d3248e5204627b0435829aa22f8
Thomas <thom.laffay@gmail.com>
2024-09-26 14:26:14 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,8 +5,7 @@ st.title("Mon :blue[_formulaire_] super cool :sunglasses:")
 st.write("Ceci est un formulaire de contact")
 
 placeholders = ["Un magnifique kebab salade tomate oignon sauce algérienne biggy", "Une enorme boule de glace sur la tour eiffel", "Un mega plateau de sushi sur le golden gate bridge"]
-user_input = st.text_input("Tapez votre prompt", placeholder=placeholders[random.randint(0,2)])
-
+user_input = st.text_input("Tapez votre prompt")
 st.write(user_input)
 
 st.image("https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/19/a3/b0/l-original-pain-maison.jpg?w=1100&h=-1&s=1")
