e113c760eeb57553ba06d7621fc6844a925897c7
Thomas <thom.laffay@gmail.com>
2024-09-26 14:37:13 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,3 +12,7 @@ st.image("https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/19/a3/b0/l-
 
 st.sidebar.title("Didier Deschamps")
 st.sidebar.video("https://youtu.be/ld-f9b3OSTg?si=cxKm-thNM78Nnusc")
+
+
+student_grade = st.selectbox("Séléctionner niveau d'étude", ["Bac", "Bac +2", "Bac+3", "Bac+5"])
+age = st.select_slider("Âge", range(0,99))
