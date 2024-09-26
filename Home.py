3adb83174dde7e9b56359b969a6aa0edd71ddf28
Thomas <thom.laffay@gmail.com>
2024-09-26 14:18:49 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import random
 
 st.title("Mon :blue[_formulaire_] super cool :sunglasses:")
 st.write("Ceci est un formulaire de contact")
-st.text_input("Tapez votre prompt", placeholder="Un magnifique kebab salade tomate oignon sauce algérienne biggy")
+
+placeholders = ["Un magnifique kebab salade tomate oignon sauce algérienne biggy", "Une enorme boule de glace sur la tour eiffel", "Un mega plateau de sushi sur le golden gate bridge"]
+st.text_input("Tapez votre prompt", placeholder=placeholders[random.randint(0,2)])
