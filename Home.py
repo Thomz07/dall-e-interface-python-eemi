08bd6d5e93bc33d3c473cd9a06ab29dcfc491394
Thomas <thom.laffay@gmail.com>
2024-09-26 14:19:46 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,4 +5,6 @@ st.title("Mon :blue[_formulaire_] super cool :sunglasses:")
 st.write("Ceci est un formulaire de contact")
 
 placeholders = ["Un magnifique kebab salade tomate oignon sauce algérienne biggy", "Une enorme boule de glace sur la tour eiffel", "Un mega plateau de sushi sur le golden gate bridge"]
-st.text_input("Tapez votre prompt", placeholder=placeholders[random.randint(0,2)])
+user_input = st.text_input("Tapez votre prompt", placeholder=placeholders[random.randint(0,2)])
+
+st.write(user_input)
